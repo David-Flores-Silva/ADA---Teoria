0c039a2db53132a1905e1ed77024cea80c63c7cb
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-07 12:34:05 -0500

    se agrego el codigo del metodo de ordenamiento de heapsort en donde contamos con tres metodos uno para realizar la separacion y otro que se encarga de ordenar entre quien es mayor

---
+++ Proyecto/HeapSort.py
@@ -0,0 +1,48 @@
+def swap(lst, i, j):
+    lst[i], lst[j], = lst[j], lst[i]
+
+
+def siftDown(lst, i, upper):
+    while(True):
+        l, r = i*2+1, i*2+2
+        if max(l, r) < upper:
+            if lst[i] >= max(lst[l], lst[r]): 
+                break
+            elif lst[l] > lst[r]:
+                swap(lst, i, l)
+                i = l
+            else:
+                swap(lst, i, r) 
+                i = r
+        
+        elif l < upper:
+            if lst[l] > lst[i]:
+                swap(lst, i, l)
+                i = l
+            else:
+                break
+        
+        elif r < upper:
+            if lst[r] > lst[i]:
+                swap(lst, i, r)
+                i = r
+            else:
+                break
+        else:
+            break
+
+def HeapSort(lst):
+    for j in range((len(lst)-2)//2, -1 , -1):
+        siftDown(lst, j, len(lst))
+
+    for end in range(len(lst)-1, 0, -1):
+        swap(lst, 0, end)
+        siftDown(lst, 0, end)
+
+lst = [2, 6, 5, 1, 3, 4]
+HeapSort(lst)
+print(lst)
+
+
+
+
